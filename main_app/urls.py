d598b7de33d2d2ed5cf6d6327c8603b059780829
sabona <sabonayusuf@gmail.com>
2021-01-08 14:42:42 -0500

    Complete Step 3.3

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/delete/<int:widget_id>/', views.delete_widget, name='delete'),
+    path('/create/', views.create_widget, name='create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Widget
+from .forms import WidgetForm
 
 # Create your views here.
 
 def index(request):
+    widgets = Widget.objects.all()
+    widget_form = WidgetForm()
+    return render(request, 'index.html', {'widgets': widgets,'widget_form':widget_form})
+
+def create_widget(request):
+    # widgets = Widget.objects.all()
+    return redirect('index')
+
+def delete_widget(request, widget_id):
     widgets = Widget.objects.all()
     return render(request, 'index.html', {'widgets': widgets})
